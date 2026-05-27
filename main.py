2e11fc74c52a6866a3e20012329e446885381702
Darcy Sprigg <darcy2946@icloud.com>
2026-05-27 14:40:38 +0700

    save

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def main():
 
-    # Firstly, lets import all video files from UCF 101 and put them into a list
+    # Firstly, lets import all video files from UCF101 and put them into a list
     path = Path('/Users/darcysprigg/Coding/Co-op summer 2026/UCF101')
     video_files = list(path.rglob('*.avi'))
     # print(video_files)
