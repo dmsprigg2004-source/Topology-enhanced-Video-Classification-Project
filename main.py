5220cc7318b68b455fd72d6f59ea46a656484d96
Darcy Sprigg <darcy2946@icloud.com>
2026-05-27 14:27:13 +0700

    Set up video importing

---
+++ main.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+def main():
+
+    # Firstly, lets import all video files from UCF 101 and put them into a list
+    path = Path('/Users/darcysprigg/Coding/Co-op summer 2026/UCF101')
+    video_files = list(path.rglob('*.avi'))
+    # print(video_files)
+
+if __name__ == "__main__":
+    main()
