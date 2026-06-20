246455922aa537155b00981fc927ea4e918ae6e2
Darcy Sprigg <darcy2946@icloud.com>
2026-06-20 15:31:56 +0700

    Added 3 channel concatenation

--- baseline_3D_CNN.py
+++ baseline_3D_CNN.py
@@ -36,14 +36,14 @@ from gudhi.representations import PersistenceImage
 num_categories = 1
 splits = {"train": 70, "val": 10, "test": 20}
 epochs = 1
-height = 56
-width = 56
+height = 112
+width = 112
 n_frames = 10
 batch_size = 8
 
 # Choose test from list
-tests = ["Baseline", "Concatenation"]
-chosen_test = tests[1]
+tests = ["Baseline", "Concatenation", "3 Channel Concatenation"]
+chosen_test = tests[2]
 
 def main():
 
@@ -73,6 +73,10 @@ def main():
     # Test concatenation based fusion if selected
     elif chosen_test == "Concatenation":
         test_concatenation_based_fusion(steps_per_epoch, validation_steps, subset_dirs, train_ds, val_ds, test_ds)
+
+    # Test 3 channel concatenation based fusion if selected
+    elif chosen_test == "3 Channel Concatenation":
+        test_3_channel_concatenation_based_fusion(steps_per_epoch, validation_steps, subset_dirs, train_ds, val_ds, test_ds)
 
     return 
 
@@ -656,18 +660,21 @@ def generate_point_clouds(video_frames):
 
     # For loop to access individual frames
     for frame in video_frames:
+
+        # If baseline test is selected, preprocess data
+        if chosen_test == "Baseline":
         
-        # Convert to NumPy ndarray
-        np_frame = frame.numpy()
+            # Convert to NumPy ndarray
+            np_frame = frame.numpy()
 
-        # Convert frame to greyscale form
-        grey_image = cv2.cvtColor(np_frame, cv2.COLOR_RGB2GRAY)
+            # Convert frame to greyscale form
+            grey_image = cv2.cvtColor(np_frame, cv2.COLOR_RGB2GRAY)
 
-        # Rescale grey image
-        grey_image = (grey_image * 255).astype(np.uint8)
+            # Rescale grey image
+            frame = (grey_image * 255).astype(np.uint8)
 
-        # Convert greyscale image to binary image
-        ret, binary_image = cv2.threshold(grey_image, 127, 255, cv2.THRESH_BINARY)
+        # Convert frame to binary image
+        ret, binary_image = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
 
         # Append binary image to list
         binary_frames.append(binary_image)
@@ -746,7 +753,7 @@ def generate_persistence_images(simplex_trees):
     return persistence_images
 
 # Function that takes a dataset and returns topological features of the data
-def tf_extraction(x_ds, name):
+def tf_extraction_ds(x_ds, name):
     
     # Initialize dictionary and count variable
     frames_dict = {}
@@ -764,7 +771,7 @@ def tf_extraction(x_ds, name):
     persistence_images_list = []
 
     # Loop through frame dictionary to create point cloud dictionary
-    for label, frames in tqdm(frames_dict.items(), desc= f"{name} - Genrating point clouds"):
+    for label, frames in tqdm(frames_dict.items(), desc= f"{name} - Generating point clouds"):
         point_cloud_dict[label] = generate_point_clouds(frames)
 
     # Loop through point cloud dictionary to create simplex tree and persistence diagram dictionaries
@@ -785,12 +792,33 @@ def tf_extraction(x_ds, name):
     # Return dicitonaries and list
     return persistence_images_list
 
+# Function that takes a list of frames and returns topological features of the data
+def tf_extraction_list(frame_list, name):
+
+    # Generate point clouds
+    print(f"{name} - Genrating point clouds...")
+    point_clouds = generate_point_clouds(frame_list)
+    print("Done")
+
+    # Generate simplex trees and persistence diagrams
+    print(f"{name} - Generating simplex trees and pesistence diagrams...")
+    simplex_trees, persistence_diagrams = generate_pds_sts(point_clouds)
+    print("Done")
+
+    # Generate persistence images
+    print(f"{name} - Generating persistence images...")
+    persistence_images_list = generate_persistence_images(simplex_trees)
+    print("Done")
+
+    # Return list of persistence images
+    return persistence_images_list
+
 # ----------------------------------- END OF TOPOLOGICAL FEATURE EXTRACTION CODE --------------------------------------
 
 # ---------------------------------------- CONCATENATION BASED FUSION CODE --------------------------------------------
 
-# Define fused_frame_generator class that generates concatenations of video frames and persistence images
-class fused_frame_generator:
+# Define Concatenated_frame_generator class that generates concatenations of video frames and persistence images
+class Concatenated_frame_generator:
 
     # __init__ function to initialize instance attributes
     def __init__(self, x_ds, pis_list):
@@ -828,6 +856,119 @@ class fused_frame_generator:
 
                 # Concatenate video frame and current persistence image tensor
                 concatenated_frame = tf.concat([frame, cur_pi_tensor], axis = -1)
+
+                # Append concatenated frame to concatenated frames list
+                concatenated_frames.append(concatenated_frame)
+                
+                # Set index for next loop
+                index += 1
+
+            # Make concatenated frames list an array
+            concatenated_frames_array = np.array(concatenated_frames)
+
+            # Yield concatenated frames array and its respective label
+            yield concatenated_frames_array, label
+
+# Funciton that splits video frames into frames representing one colour channel each
+def split_frames(x_ds):
+
+    # Initialize lists
+    red = []
+    blue = []
+    green = []
+
+    # Loop through frames within inputted dataset
+    for frames, label in x_ds:
+
+        # Loop through individual frames
+        for frame in frames:
+
+            # Convert to NumPy ndarray
+            np_frame = frame.numpy()
+
+            # Rescale image
+            np_frame = (np_frame * 255).astype(np.uint8)
+
+            # Split into colour channels
+            r, g, b = cv2.split(np_frame)
+
+            # Append channels to lists
+            red.append(r)
+            blue.append(b)
+            green.append(g)
+    
+    # Define all black channel with the same shape as colour channels
+    black = np.zeros(red[0].shape, np.uint8)
+
+    # Loop through colour channel lists
+    for i in range(len(red)):
+        
+        # Select specific channel
+        r = red[i]
+        b = blue[i]
+        g = green[i]
+
+        # Merge colour channels with fully black channel
+        new_r = cv2.merge([r, black, black])
+        new_g = cv2.merge([black, g, black])
+        new_b = cv2.merge([black, black, b])
+
+        # Update channels to new values
+        red[i] = new_r
+        blue[i] = new_b
+        green[i] = new_g
+
+    # Return lists
+    return red, green, blue
+
+class Three_channel_concatenated_frame_generator:
+
+    # __init__ function to initialize instance attributes
+    def __init__(self, x_ds, red_pis_list, green_pis_list, blue_pis_list):
+        self.x_ds = x_ds
+        self.red_pis_list = red_pis_list
+        self.green_pis_list = green_pis_list
+        self.blue_pis_list = blue_pis_list
+
+    # __call__ function that yields video frames with their respective label
+    def __call__(self):
+        
+        # Initialize index tracker
+        index = 0
+
+        # Loop through frames and labels within inputted dataset
+        for frames, label in self.x_ds:
+            
+            # Initialize list of concatenated frames
+            concatenated_frames = []
+
+            # Loop through frames
+            for frame in frames:
+                
+                # Define current persistence images with current index
+                red_cur_pi = self.red_pis_list[index]
+                green_cur_pi = self.green_pis_list[index]
+                blue_cur_pi = self.blue_pis_list[index]
+
+                # If current persistence image is "None," set current persistence image tensor to a tensor of desired shape filled with zeros
+                if red_cur_pi is None or green_cur_pi is None or blue_cur_pi is None:
+                    cur_pi_tensor = tf.zeros((height,width,3), dtype = tf.float32)
+
+                    # Concatenate video frame and current persistence image tensor
+                    concatenated_frame = tf.concat([frame, cur_pi_tensor], axis = -1)
+
+                else:
+                    # Convert current persistence images to tensors
+                    red_cur_pi_tensor = tf.convert_to_tensor(red_cur_pi, dtype = tf.float32)
+                    green_cur_pi_tensor = tf.convert_to_tensor(green_cur_pi, dtype = tf.float32)
+                    blue_cur_pi_tensor = tf.convert_to_tensor(blue_cur_pi, dtype = tf.float32)
+
+                    # Reshape tensors to desired shape
+                    red_cur_pi_tensor = tf.reshape(red_cur_pi_tensor, [height, width, 1])
+                    green_cur_pi_tensor = tf.reshape(green_cur_pi_tensor, [height, width, 1])
+                    blue_cur_pi_tensor = tf.reshape(blue_cur_pi_tensor, [height, width, 1])
+
+                    concatenated_frame = tf.concat([frame, red_cur_pi_tensor, green_cur_pi_tensor, blue_cur_pi_tensor], axis = -1)
 
                 # Append concatenated frame to concatenated frames list
                 concatenated_frames.append(concatenated_frame)
@@ -908,19 +1049,19 @@ def test_baseline_model(steps_per_epoch, validation_steps, subset_dirs, train_ds
 def test_concatenation_based_fusion(steps_per_epoch, validation_steps, subset_dirs, train_ds, val_ds, test_ds):
 
     # Get topological features from the datasets
-    train_persistence_images_list = tf_extraction(train_ds, "Training")
-    val_persistence_images_list = tf_extraction(val_ds, "Validation")
-    test_persistence_images_list = tf_extraction(test_ds, "Test")
+    train_persistence_images_list = tf_extraction_ds(train_ds, "Training")
+    val_persistence_images_list = tf_extraction_ds(val_ds, "Validation")
+    test_persistence_images_list = tf_extraction_ds(test_ds, "Test")
     
     # Define output signature
     output_signature = (tf.TensorSpec(shape = (None, None, None, 4), dtype = tf.float32), tf.TensorSpec(shape = (), dtype = tf.int16))
 
     # Generate training, validation and testing datasets
-    train_concatenated_frames = tf.data.Dataset.from_generator(fused_frame_generator(train_ds, train_persistence_images_list), 
+    train_concatenated_frames = tf.data.Dataset.from_generator(Concatenated_frame_generator(train_ds, train_persistence_images_list), 
                                                                 output_signature = output_signature)
-    val_concatenated_frames = tf.data.Dataset.from_generator(fused_frame_generator(val_ds, val_persistence_images_list), 
+    val_concatenated_frames = tf.data.Dataset.from_generator(Concatenated_frame_generator(val_ds, val_persistence_images_list), 
                                                                 output_signature = output_signature)
-    test_concatenated_frames = tf.data.Dataset.from_generator(fused_frame_generator(test_ds, test_persistence_images_list), 
+    test_concatenated_frames = tf.data.Dataset.from_generator(Concatenated_frame_generator(test_ds, test_persistence_images_list), 
                                                                 output_signature = output_signature)
 
     # Make versions of datasets that repeat for training
@@ -934,6 +1075,95 @@ def test_concatenation_based_fusion(steps_per_epoch, validation_steps, subset_di
 
     # Define input shape
     input_shape = (None, n_frames, height, width, 4)
+
+    # Call function to create the 3D CNN model
+    model = create_3D_CNN(train_concatenated_frames, input_shape)
+
+    # Prepare model for training with the Adam optimizer and SparseCategoricalCrossentropy loss function
+    model.compile(loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    optimizer = keras.optimizers.legacy.Adam(learning_rate = 0.0001), metrics = ['accuracy'])
+
+    # Train the model and obtain model history using model.fit()
+    history = model.fit(x = repeat_train_concatenated_frames, epochs = epochs, validation_data = repeat_val_concatenated_frames, 
+                        steps_per_epoch = steps_per_epoch, validation_steps = validation_steps)
+
+    # Call function to plot history of model training performance
+    plot_history(history)
+
+    # Evaluate model to get accuracy and loss values
+    model_accuracy_and_loss = model.evaluate(test_concatenated_frames, return_dict=True)
+
+    # Obtain rounded model accuracy value
+    model_accuracy = round(model_accuracy_and_loss["accuracy"], 2)
+
+    # Use FrameGenerator class to obtain class labels from training data
+    fg = FrameGenerator(subset_dirs['train'], n_frames, training=True)
+    labels = list(fg.class_ids_for_name.keys())
+
+    # Call funciton to get actual and predicted values from the training dataset, then plot confusion matrix
+    actual, predicted = get_actual_predicted_labels(train_concatenated_frames, model)
+    plot_confusion_matrix(actual, predicted, labels, 'training')
+
+    # Call funciton to get actual and predicted values from the test dataset, then plot confusion matrix
+    actual, predicted = get_actual_predicted_labels(test_concatenated_frames, model)
+    plot_confusion_matrix(actual, predicted, labels, 'test')
+
+    # Call function to calculate precision and recall values
+    precision, recall = calculate_precision_recall(actual, predicted, labels)
+
+    # Call function to calculate F1 scores
+    F1_scores = calculate_F1_scores(precision, recall)
+
+    # Call function to print classificaiton metrics
+    print_classification_metrics(model_accuracy, precision, recall, F1_scores)
+
+    return
+
+# Function to test 3 channel concatenation based fusion
+def test_3_channel_concatenation_based_fusion(steps_per_epoch, validation_steps, subset_dirs, train_ds, val_ds, test_ds):
+
+    # Split frames into images representing single colour channels
+    red_train_list, green_train_list, blue_train_list = split_frames(train_ds)
+    red_val_list, green_val_list, blue_val_list = split_frames(val_ds)
+    red_test_list, green_test_list, blue_test_list = split_frames(test_ds)
+
+    # Get topological features of training data
+    red_train_pi_list = tf_extraction_list(red_train_list, "Training, red")
+    green_train_pi_list = tf_extraction_list(green_train_list, "Training, green")
+    blue_train_pi_list = tf_extraction_list(blue_train_list, "Training, blue")
+
+    # Get topological features of validaiton data
+    red_val_pi_list = tf_extraction_list(red_val_list, "Validation, red")
+    green_val_pi_list = tf_extraction_list(green_val_list, "Validation, green")
+    blue_val_pi_list = tf_extraction_list(blue_val_list, "Validation, blue")
+
+    # Get topological features of testing data
+    red_test_pi_list = tf_extraction_list(red_test_list, "Test, red")
+    green_test_pi_list = tf_extraction_list(green_test_list, "Test, green")
+    blue_test_pi_list = tf_extraction_list(blue_test_list, " Test, blue")
+
+    # Define output signature
+    output_signature = (tf.TensorSpec(shape = (None, None, None, 6), dtype = tf.float32), tf.TensorSpec(shape = (), dtype = tf.int16))
+
+    # Generate training, validation, and testing datasets
+    train_concatenated_frames = tf.data.Dataset.from_generator(Three_channel_concatenated_frame_generator(train_ds, red_train_pi_list, 
+                                                            green_train_pi_list, blue_train_pi_list), output_signature = output_signature)
+    val_concatenated_frames = tf.data.Dataset.from_generator(Three_channel_concatenated_frame_generator(val_ds, red_val_pi_list, 
+                                                            green_val_pi_list, blue_val_pi_list), output_signature = output_signature)
+    test_concatenated_frames = tf.data.Dataset.from_generator(Three_channel_concatenated_frame_generator(test_ds, red_test_pi_list, 
+                                                            green_test_pi_list, blue_test_pi_list), output_signature = output_signature)
+    
+    # Make versions of datasets that repeat for training
+    repeat_train_concatenated_frames = train_concatenated_frames.repeat().batch(batch_size)
+    repeat_val_concatenated_frames = val_concatenated_frames.repeat().batch(batch_size)
+
+    # Batch data into desired sizes
+    train_concatenated_frames = train_concatenated_frames.batch(batch_size)
+    val_concatenated_frames = val_concatenated_frames.batch(batch_size)
+    test_concatenated_frames = test_concatenated_frames.batch(batch_size)
+
+    # Define input shape
+    input_shape = (None, n_frames, height, width, 6)
 
     # Call function to create the 3D CNN model
     model = create_3D_CNN(train_concatenated_frames, input_shape)
