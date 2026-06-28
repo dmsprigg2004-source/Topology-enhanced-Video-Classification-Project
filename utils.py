57a5647fc3b9f32883fbfe97c097274d949871e6
Darcy Sprigg <darcy2946@icloud.com>
2026-06-28 09:16:32 +0700

    Updated confusion matrix settings

--- utils.py
+++ utils.py
@@ -533,8 +533,8 @@ def plot_confusion_matrix(actual, predicted, labels, ds_type):
     # Generate confusion matrix
     cm = tf.math.confusion_matrix(actual, predicted)
     ax = sns.heatmap(cm, annot=True, fmt='g')
-    sns.set(rc={'figure.figsize':(12, 12)})
-    sns.set(font_scale=1.4)
+    sns.set_theme(rc={'figure.figsize':(20, 22)})
+    sns.set_theme(font_scale=1.5)
     ax.set_title('Confusion matrix of action recognition for ' + ds_type)
     ax.set_xlabel('Predicted Action')
     ax.set_ylabel('Actual Action')
